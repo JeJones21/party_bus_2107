cfabbdddbc80a8b5a256db9e4d5c9d5d0a1f7e4a
Jessica Jones <daddehjones@gmail.com>
2021-07-07 16:48:25 -0400

    Create methods for add_passenger and yell_at_passengers.

--- lib/bus.rb
+++ lib/bus.rb
@@ -1,4 +1,24 @@
 class Bus
+  attr_reader :name,
+              :capacity,
+              :passengers
 
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @passengers = []
+  end
+
+  def add_passenger(human)
+    @passengers << human
+  end
+
+  def yell_at_passengers
+    @passengers.map do |passenger|
+      passenger.upcase
+    end
+  end
+
+  
 
 end
